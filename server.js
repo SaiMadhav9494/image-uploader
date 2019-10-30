515f9fb245b082065788963e46d5ffac2a3c18ff
Sai Madhav Perumalla <sai.madhav@scroll.network>
2019-10-30 18:47:46 -0400

    Set path

--- server.js
+++ server.js
@@ -6,14 +6,14 @@ const cors = require('cors');
 
 const app = express();
 
-app.options('*', cors());
+app.options('*', cors())
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/beeble'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+res.sendFile(path.join(__dirname+'/dist/beeble/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
